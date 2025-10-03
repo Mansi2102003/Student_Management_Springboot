977db041223dedb8a8f9d90b63520ff8fa0c5dee
Mansi Thakkar <mthakkar@logilite.com>
2025-10-03 13:25:55 +0530

    "Student class created to set and get values of student"

---
+++ src/main/java/com/example/studentmanagement/model/Student.java
@@ -0,0 +1,71 @@
+package com.example.studentmanagement.model;
+
+public class Student {
+	int id;
+    String name;
+    String course;
+    String email;
+    String phno;
+    String address;
+ 
+	public Student(int id, String name, String course, String email, String phno, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.course = course;
+		this.email = email;
+		this.phno = phno;
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhno() {
+		return phno;
+	}
+
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	 
+     
+}
