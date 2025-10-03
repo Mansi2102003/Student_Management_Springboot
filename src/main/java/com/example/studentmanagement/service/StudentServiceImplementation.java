108781cfccd55b66e0dfa7d35d10680f797efa08
Mansi Thakkar <mthakkar@logilite.com>
2025-10-03 16:41:31 +0530

    "Implemented business logic for add student data into database"

---
+++ src/main/java/com/example/studentmanagement/service/StudentServiceImplementation.java
@@ -0,0 +1,20 @@
+package com.example.studentmanagement.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.studentmanagement.dao.StudentDao;
+import com.example.studentmanagement.model.Student;
+
+public class StudentServiceImplementation implements StudentService {
+	
+	//Autowired annotation means it tells spring to wired studenDao bean automatically
+	@Autowired
+	StudentDao studentDao;
+	
+	//It contains core business logic is written
+	@Override
+	public Student addStudent(Student student) {  //Take Student object as input
+		studentDao.save(student);   //Call studentsdao's method to save into database
+		return student;
+	}
+}
