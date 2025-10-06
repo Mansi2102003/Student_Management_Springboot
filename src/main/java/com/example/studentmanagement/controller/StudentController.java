730f144463d5fb8eab5870af6cd4837c5b5dbbbb
Mansi Thakkar <mthakkar@logilite.com>
2025-10-06 14:12:30 +0530

    "Created Method inside student controller to map request"

--- src/main/java/com/example/studentmanagement/controller/StudentController.java
+++ src/main/java/com/example/studentmanagement/controller/StudentController.java
@@ -2,7 +2,10 @@ package com.example.studentmanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +24,17 @@ public class StudentController {
   public ResponseEntity<Student> createStudent(@RequestBody Student student) { //takes jSON body and convert into student object automatically 
 	    return ResponseEntity.ok(studentService.addStudent(student)); //calls add student method to add user
 	}
+  
+  
+  @GetMapping("/{id}")
+  public ResponseEntity<Student> getStudentById(@PathVariable int id) {
+      return ResponseEntity.ok(studentService.getStudentById(id));
+  }
+  
+  @PutMapping("/{id}")
+  public ResponseEntity<Student> updateStudent(@PathVariable int id, @RequestBody Student student) throws Exception{
+      student.setId(id);
+      return ResponseEntity.ok(studentService.updateStudentInfo(student));
+  }
+  
 }
