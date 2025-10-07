f337a065676593c87c75d239ba5e393b93f4983a
Mansi Thakkar <mthakkar@logilite.com>
2025-10-07 13:19:57 +0530

    "Adding Home Page for student management student"

--- src/main/java/com/example/studentmanagement/controller/StudentController.java
+++ src/main/java/com/example/studentmanagement/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,9 @@ import com.example.studentmanagement.model.Student;
 import com.example.studentmanagement.service.StudentService;
 
 @RestController // tells springs that it handles HTTP requests and return response
-@RequestMapping("/api/students") // sets base url for endpoint inside controller
+@RequestMapping("/api/students")// sets base url for endpoint inside controller
+
+@CrossOrigin(origins = "http://localhost:8080")
 public class StudentController {
 	
   @Autowired 
